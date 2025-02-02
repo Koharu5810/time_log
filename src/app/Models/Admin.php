e1fba98543aada126139152d5042fce683cd7c91
koharu <le2.lsb108@gmail.com>
2025-02-02 15:24:09 +0900

    Admin、Memberモデル修正

--- src/app/Models/Admin.php
+++ src/app/Models/Admin.php
@@ -8,4 +8,18 @@ use Illuminate\Database\Eloquent\Model;
 class Admin extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+    ];
+    protected $hidden = [
+        'password',
+    ];
+
+    public function attendance_requests()
+    {
+        return $this->hasMany(AttendanceRequest::class, 'admin_id');
+    }
 }
--- src/app/Models/Member.php
+++ src/app/Models/Member.php
@@ -4,8 +4,37 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Member extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'uuid',
+        'name',
+        'email',
+        'password',
+    ];
+    protected $hidden = [
+        'password',
+    ];
+    // UUIDを作自動成
+    protected static function booted()
+    {
+        static::creating(function ($member) {
+            if (empty($member->uuid)) {
+                $member->uuid = (string) Str::uuid();
+            }
+        });
+    }
+
+    public function attendances()
+    {
+        return $this->hasMany(Attendance::class, 'member_id');
+    }
+    public function attendance_requests()
+    {
+        return $this->hasMany(AttendanceRequest::class, 'member_id');
+    }
 }
