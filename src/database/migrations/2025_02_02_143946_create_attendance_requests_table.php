b7a65051b028414aec5274e5dd9d955800808f6a
koharu <le2.lsb108@gmail.com>
2025-02-02 14:40:14 +0900

    AttendanceRequestモデル、マイグレーションファイル作成

--- src/database/migrations/2025_02_02_143946_create_attendance_requests_table.php
+++ src/database/migrations/2025_02_02_143946_create_attendance_requests_table.php
@@ -11,11 +11,8 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('break_times', function (Blueprint $table) {
+        Schema::create('attendance_requests', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('attendance_id')->constrained()->cascadeOnDelete();
-            $table->time('break_time_start');
-            $table->time('break_time_end');
             $table->timestamps();
         });
     }
@@ -25,6 +22,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('break_times');
+        Schema::dropIfExists('attendance_requests');
     }
 };
