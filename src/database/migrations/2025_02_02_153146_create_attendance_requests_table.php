2d37de917f07b68df3ea176825afd6d501f5ce9d
koharu <le2.lsb108@gmail.com>
2025-02-02 15:10:39 +0900

    マイグレーション実行

---
+++ src/database/migrations/2025_02_02_153146_create_attendance_requests_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('attendance_requests', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('member_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('attendance_id')->constrained()->cascadeOnDelete();
+            $table->enum('request_type', ['修正', '申請'])->default('修正');
+            $table->time('requested_clock_in');
+            $table->time('requested_clock_end');
+            $table->string('requested_remarks', 255);
+            $table->enum('status', ['承認待ち', '承認済み'])->default('承認待ち');
+            $table->foreignId('admin_id')->constrained()->cascadeOnDelete();
+            $table->timestamp('approved_at');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('attendance_requests');
+    }
+};
