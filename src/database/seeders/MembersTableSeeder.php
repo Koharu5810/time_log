5dcf1446d7e3899eeefb106e618ae945e96fa935
koharu <le2.lsb108@gmail.com>
2025-02-08 12:23:19 +0900

    Membersシーダファイル修正

--- src/database/seeders/MembersTableSeeder.php
+++ src/database/seeders/MembersTableSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Member;
 
 class MembersTableSeeder extends Seeder
 {
@@ -12,6 +13,18 @@ class MembersTableSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $param = [
+            'name' => 'スタッフ1',
+            'email' => 'staff1@test.com',
+            'password' => 'password',
+        ];
+        Member::create($param);
+
+        $param = [
+            'name' => 'スタッフ2',
+            'email' => 'staff2@test.com',
+            'password' => 'password',
+        ];
+        Member::create($param);
     }
 }
