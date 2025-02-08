c78bf601872078faca37ce69483daef87db0ea2c
koharu <le2.lsb108@gmail.com>
2025-02-08 12:01:32 +0900

    Adminsシーダファイル修正

--- src/database/seeders/AdminsTableSeeder.php
+++ src/database/seeders/AdminsTableSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Admin;
 
 class AdminsTableSeeder extends Seeder
 {
@@ -12,6 +13,18 @@ class AdminsTableSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $param = [
+            'name' => '管理者ユーザ1',
+            'email' => 'admin1@test.com',
+            'password' => 'password',
+        ];
+        Admin::create($param);
+
+        $param = [
+            'name' => '管理者ユーザ2',
+            'email' => 'admin2@test.com',
+            'password' => 'password',
+        ];
+        Admin::create($param);
     }
 }
